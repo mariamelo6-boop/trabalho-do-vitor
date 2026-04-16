527217908f83f641c4949d9f739ed3991553cd17
liviaestrela <livia.estrela@estudante.ifms.edu.br>
2026-04-16 09:09:07 -0400

    alteração do js com o html

--- livia.js
+++ livia.js
@@ -1,18 +1,19 @@
-const filmes = document.querySelectorAll(".model");
+const filmes = document.querySelectorAll("model");//  pega tds os elemnys com model
 const totalf = document.getElementById("total");
-let total = 0;
+const finalizar = document.getElementById("finalizar");
 
+let total = 0;
 filmes.forEach(filme => {
     const btnMais = filme.querySelector(".mais");
     const btnMenos = filme.querySelector(".menos");
-    const qtdElemento = filme.querySelector(".qtd");
-    let quantidade = 0;
-    const preco = Number(filme.getAttribute("preco"));
+    const qtdElemento = filme.querySelector("qntd");
 
+    let quantidade = 0;
+    const preco = Number(filme.getAttribute("preco"));// converte o valor em text para numero
     btnMais.addEventListener("click", () => {
         quantidade++;
         total += preco;
-
+// att o preço para aparecer na tela
         qtdElemento.textContent = quantidade;
         atualizarTotal();
     });
